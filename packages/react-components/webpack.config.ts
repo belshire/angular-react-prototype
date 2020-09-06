a3a9c3f2848676685017b60922e084e89da63340
Blake Elshire <blake.elshire@gmail.com>
2020-09-05 23:06:19 -0500

    react, eslint, ts, webpack config setup

---
+++ packages/react-components/webpack.config.ts
@@ -0,0 +1,42 @@
+import path from "path";
+import webpack from "webpack";
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+const config: webpack.Configuration = {
+  entry: "./components/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: "./src/**/*",
+      },
+    }),
+  ],
+};
+
+export default config;
